19298d82727da3fc42c76e176bacabdacc16acaa
chandjenAC <chandjen.ac@tallyx.com>
2021-05-20 18:34:29 +0530

    Focus search input on initial load

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,4 +1,5 @@
 import "./search.scss";
+import { useRef, useEffect } from "react";
 
 const Search = ({
   onSubmitSearch,
@@ -6,6 +7,12 @@ const Search = ({
   handleChangeSearch,
   disableSubmit,
 }) => {
+  const inputRef = useRef(null);
+  
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
   return (
     <form onSubmit={onSubmitSearch}>
       <div className="searchContainer">
@@ -15,6 +22,7 @@ const Search = ({
             type="text"
             title="searchInput"
             placeholder="Search github users"
+            ref={inputRef}
             value={searchText}
             onChange={handleChangeSearch}
           />
