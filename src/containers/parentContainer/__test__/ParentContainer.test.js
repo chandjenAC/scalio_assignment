17919dc157b2844afa5a9b024c4e20c3d15bd5ac
chandjenAC <chandjen.ac@tallyx.com>
2021-05-23 20:35:48 +0530

    done

--- src/containers/parentContainer/__test__/ParentContainer.test.js
+++ src/containers/parentContainer/__test__/ParentContainer.test.js
@@ -1,19 +1,8 @@
 import ParentContainer from "../ParentContainer";
-import { fireEvent, render, waitFor } from "@testing-library/react";
+import { render } from "@testing-library/react";
 
 describe("Parent Container", () => {
   test("renders without crashing", () => {
     render(<ParentContainer />);
   });
-
-//   test("fetch user data for entered keyword", async () => {
-//     const fetchUsersData = jest.fn();
-//     fetchUsersData.mockResolvedValueOnce({});
-//     const { getByTestId, getByText } = render(<ParentContainer />);
-//     const inputEl = getByTestId("searchInput");
-//     const submitBtnEl = getByTestId("submit-btn");
-//     fireEvent.change(inputEl, { target: { value: "Test Username" } });
-//     fireEvent.click(submitBtnEl);
-//     await waitFor(() => getByText());
-//   });
 });
